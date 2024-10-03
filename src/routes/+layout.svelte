ee8c11972383ff02556854a92f3a4207fb7470db
Teker932 <70298466+eputnam0522@users.noreply.github.com>
2024-10-03 16:35:54 +0000

    Start Svelte shi

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import "../app.css";
+</script>
+<div id="nav" class="bg-slate-900 text-slate-50">
+    <h1 class="text-4xl text-green-500">Terminal</h1>
+    <h1 class="text-4m"><a href="/">Home</a> | <a href="/games">Games</a> | <a href="/about">About</a></h1>
+</div>
+<slot />
+<div id="nav" class="bg-slate-900 text-slate-700 text-center">
+    <p>With ❤️, from CO</p>
+    <p>(C) 2024</p>
+</div>
+<style>
+    * :global() {
+        background-color: bg-slate-900;
+        color: text-slate-50;
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<br>
+<h1 class="text-2xl">About Us</h1>
+<p>Terminal emerged from the merging of two projects: Terminal and Spielaffin. We have our origins in a project called "MediaApe,", and unblocked games site created in 2021 and blocked in 2023. It had a good 2 year run, but the founder, teker, had large ambitions</p>
